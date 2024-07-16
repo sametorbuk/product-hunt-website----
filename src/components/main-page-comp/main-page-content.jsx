838bbd53e6dc56046a236433f8875e5e69642f10
sametorbuk <orbuk60@gmail.com>
2024-07-16 16:43:23 +0300

    düzenleme

--- src/components/main-page-comp/main-page-content.jsx
+++ src/components/main-page-comp/main-page-content.jsx
@@ -5,6 +5,7 @@ import "./main-page-content.css"
 import DailyProductComp from "../product-comp/daily-product-comp/daily-product-comp"
 import RecentShoutoutProductComp from '../recent-shoutout-product/recent-shoutout-product-comp';
 
+import { recentProduct } from '../../recent-product-veri.js';
 
 export default function MainPageContent() {
 
@@ -15,8 +16,11 @@ export default function MainPageContent() {
         <div className="main-page-content-left">
             <div className="main-page-content-left-row1">
             <h2 style={{flex:"0.726"}}>Top Products Launching Today</h2>
+
+            <div className='main-page-content-left-row1-btn'>
             <button className="main-page-content-left-row1">Featured</button>
             <button className="main-page-content-left-row1" >All</button>
+            </div>
             </div>
         
         <DailyProductComp/>
@@ -24,14 +28,19 @@ export default function MainPageContent() {
         </div>
         <div className="main-page-content-right">
 
-         <RecentShoutoutProductComp/>
-         <RecentShoutoutProductComp/>
-        <RecentShoutoutProductComp/>
+          <h3>Recent Shoutouts</h3>
+
+        {recentProduct.map((product)=>{
+          
+          return <RecentShoutoutProductComp product={product}/>
+
+        })}
+        
+        
         </div>
 </div>
         
-        
-        
+      
         
         
         
--- src/components/recent-shoutout-product/recent-shoutout-product-comp.jsx
+++ src/components/recent-shoutout-product/recent-shoutout-product-comp.jsx
@@ -3,19 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowsSplitUpAndLeft } from "@fortawesome/free-solid-svg-icons"
 
 
-export default function RecentShoutoutProductComp(){
-
-
+export default function RecentShoutoutProductComp(props){
+ const {product}=props
+const {name , exp1 , logo ,exp2 , platform}=product
+    
     return(<>
     
     <div className="recent-product-comp-div">
      <div className="recent-product-info">
           
-          <img className="recent-product-photo" src="https://ph-files.imgix.net/6c3ee8b9-7869-4500-9b26-8121c23d8ef2.jpeg?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=48&h=48&fit=crop&dpr=2" alt="" />
+          <img className="recent-product-photo" src={logo} alt="" />
         
             <div className="recent-product-info-explanation">
-               
-               <p>Sugar free: Food Scanner —</p>
+               <p style={{fontWeight:"bold"}}>{name}--</p>
+               <p>{exp1}</p>
                
 
           </div>
@@ -24,8 +25,8 @@ export default function RecentShoutoutProductComp(){
      </div>
      <div className="recent-product-explanation">
 
-         <p>Shouted out <FontAwesomeIcon icon={faArrowsSplitUpAndLeft} />Heroku</p>
-         <p className="recent-product-explanation" >Allows quickly testing multiple open models side-by-side.</p>
+         <p>Shouted out <img className="platform-photo" src={platform} alt="" /> Heroku</p>
+         <p className="recent-product-explanation" >{exp2}</p>
       
 
      </div>
