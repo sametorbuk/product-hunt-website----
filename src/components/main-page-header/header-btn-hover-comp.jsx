cf034f3f0e724cc7d0ed4746acaa89af845e09f7
sametorbuk <orbuk60@gmail.com>
2024-07-17 12:53:06 +0300

    düzenleme

--- src/components/main-page-header/header-btn-hover-comp.jsx
+++ src/components/main-page-header/header-btn-hover-comp.jsx
@@ -1,39 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './header-btn-hover-comp.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faClock,
-  faRocket,
-  faMessage,
-  faFire,
-  faCalendar,
-} from '@fortawesome/free-solid-svg-icons';
 
 export default function HoveredComp(props) {
   const { array, stateChange } = props;
 
   return (
-    <>
-      <div
-        onMouseEnter={() => stateChange(true)}
-        onMouseLeave={() => stateChange(false)}
-        className={styles.hoverComp}
-      >
-        {array.map((item) => {
-          return (
-            <div className={styles.row1}>
-              <FontAwesomeIcon
-                style={{ color: item.color }}
-                className={styles.icon1}
-                icon={item.code}
-              />
-              <div className={styles.row1Right}>
-                <p>{item.ex1}</p>
-                <p>{item.ex2}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div
+      onMouseEnter={() => stateChange(true)}
+      onMouseLeave={() => stateChange(false)}
+      className={styles.hoverComp}
+    >
+      {array.map((item, index) => (
+        <div key={index} className={styles.row1}>
+          <FontAwesomeIcon
+            style={{ color: item.color }}
+            className={styles.icon1}
+            icon={item.code}
+          />
+          <div className={styles.row1Right}>
+            <p>{item.ex1}</p>
+            <p>{item.ex2}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
+
+HoveredComp.propTypes = {
+  array: PropTypes.arrayOf(
+    PropTypes.shape({
+      color: PropTypes.string.isRequired,
+      code: PropTypes.object.isRequired,
+      ex1: PropTypes.string.isRequired,
+      ex2: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  stateChange: PropTypes.func.isRequired,
+};
