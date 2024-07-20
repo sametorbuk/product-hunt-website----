a437f12d4bb20ad36463f07a9d6630c58daa7cda
sametorbuk <orbuk60@gmail.com>
2024-07-20 10:26:13 +0300

    düzenleme

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+import path from 'path'; // path modülünü import ediyoruz
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: path.resolve('public/sowhat.com-key.pem'), // SSL anahtar dosyasının yolu
+      cert: path.resolve('public/sowhat.com.pem'),    // SSL sertifika dosyasının yolu
+    },
+  },
 });
